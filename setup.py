fa7d511f5563c2cd61fb6afd2fe9d89f6f1a13e5
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2023-10-28 20:19:15 +0530

    Update debian packaging (#68)
    - Precompile gschema for other packaging
    - Use dh-python to determine
      python depends
    - Use namespace to find package files
    - Use mamolinux repo for bugs
    - Add python depends to setup.cfg

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+import glob, os
+import distutils.command.install_data
 
-import glob
-
+from distutils.log import info
 from setuptools import setup
 from subprocess import check_output
 
@@ -18,8 +19,23 @@ with open("src/ThemeManager/VERSION", "w") as f:
         version = version.split('~')[0]
     f.write("%s" % version)
 
-setup(
-data_files=[('share/applications', glob.glob("data/applications/*.desktop")),
-            ('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
-            ]
+gschema_dir_suffix = 'share/glib-2.0/schemas'
+
+class install_data(distutils.command.install_data.install_data):
+    def run(self):
+        # Python 3 'super' call.
+        super().run()
+        
+        # Compile '*.gschema.xml' to update or create 'gschemas.compiled'.
+        info("compiling gsettings schemas")
+        # Use 'self.install_dir' to build the path, so that it works
+        # for both global and local '--user' installs.
+        gschema_dir = os.path.join(self.install_dir, gschema_dir_suffix)
+        self.spawn(["glib-compile-schemas", gschema_dir])
+
+setup(data_files=[('share/applications', glob.glob("data/applications/*.desktop")),
+        ('share/icons/hicolor/scalable/apps', glob.glob("data/icons/*")),
+        (gschema_dir_suffix, glob.glob("data/*.xml"))
+        ],
+        cmdclass = {'install_data': install_data}
 )
